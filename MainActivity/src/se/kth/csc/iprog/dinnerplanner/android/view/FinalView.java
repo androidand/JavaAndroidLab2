b3bdb4218889b9d45fb11997cf990b8ad1699fb8
androidand <sylvan@kth.se>
2014-02-20 13:07:59 +0100

    Recreated the FinalView xml.

---
+++ MainActivity/src/se/kth/csc/iprog/dinnerplanner/android/view/FinalView.java
@@ -0,0 +1,23 @@
+package se.kth.csc.iprog.dinnerplanner.android.view;
+
+import se.kth.csc.iprog.dinnerplanner.android.R;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.res.Resources;
+
+
+public class FinalView {
+
+	View view;	
+
+	public FinalView (View view){
+
+		this.view = view;
+	}
+
+
+}
+
+
